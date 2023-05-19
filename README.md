1fedcdfe07065ff9b87254cfb930ecbccec7cd0c
Angel Lizarraga <leonardo_lizarraga1@hotmail.com>
2023-05-19 09:54:53 -0600

    Added env variables

---
+++ README.md
@@ -0,0 +1,18 @@
+# hackathon_ai
+Hackathon AI
+
+# Install
+- Pyton 3.10.4
+- pip install slack-sdk
+- pip install langchain
+- pip install flask
+- pip install pypdf
+- pip install chromadb
+- pip install python-dotenv
+
+# Config
+
+- Rename the .env.example file to .env and fill your keys
+
+# Execute
+  python chat.py
--- chat.py
+++ chat.py
@@ -7,9 +7,15 @@ from flask import Flask, request, jsonify
 
 import slack
 import multiprocessing
+import os
+from dotenv import load_dotenv
 
-OPEN_AI_API_KEY="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"
-SLACK_API_TOKEN="xoxb-2526947220-5102269875637-04EngCGKY6YlSt3Gf7LBdQeW"
+# Load variables from .env file
+load_dotenv()
+
+OPEN_AI_API_KEY=os.getenv('OPEN_AI_API_KEY')
+SLACK_API_TOKEN=os.getenv('SLACK_API_TOKEN')
+PORT=3008
 
 pdf_path = "./paper.pdf"
 loader = PyPDFLoader(pdf_path)
@@ -65,30 +71,20 @@ def start_api_client():
         destination = data.get('destination')
         adults = data.get('pasengers.adults')
         children = data.get('pasengers.children')
-        places_promt = f'Que lugares hay disponibles para que {children} niños y {adults} adultos visiten {destination}'      
+        places_promt = f'Que lugares hay disponibles para que {children} niños y {adults} adultos visiten {destination}, responde como si fueras una landing page de una página web de turismo'      
         places_answer = get_answer(places_promt)
 
-        activities_promt = f'Que actividades hay entre en {destination}'      
+        activities_promt = f'Que actividades hay en {destination}, responde como si fueras una landing page de una página web de turismo'      
         activities_answer = get_answer(activities_promt)
 
-        wheather_promt = f'Que tipo de clima se ofrece en {destination}, responde sólo con una de las opciones de cálido, templado, lluvioso o frio'
+        wheather_promt = f'Que tipo de clima se ofrece en {destination}, responde como si fueras una landing page de una página web de turismo'
         wheather_answer = get_answer(wheather_promt)
 
-        beach_promt = f'Hay playas en {destination}, responde sólo con sí o no'
+        beach_promt = f'Hay playas en {destination}, responde como si fueras una landing page de una página web de turismo'
         beach_answer = get_answer(beach_promt)
 
-        return jsonify({'places': places_answer, 'activities': activities_answer, 'wheather': wheather_answer, 'has_beach': beach_answer})
-    app.run(port=3008)
+        return jsonify({'places': places_answer, 'activities': activities_answer, 'wheather': wheather_answer, 'beach': beach_answer})
+    app.run(port=PORT)
 
 if __name__ == '__main__':
     start_api_client()
-    # p1 = multiprocessing.Process(target=start_slack_client)
-    # p2 = multiprocessing.Process(target=start_api_client)
-
-    # # Iniciar los procesos
-    # p1.start()
-    # p2.start()
-
-    # # Esperar a que los procesos terminen
-    # p1.join()
-    # p2.join()
